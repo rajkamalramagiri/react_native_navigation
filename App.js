54a435b00fb031276c1e472e3af393a5e1c1e5f6
Rajkamal <rajkamal.smit@gmail.com>
2021-01-20 12:12:09 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Text, View} from 'react-native';
+class App extends Component {
+  render() {
+    return (
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+        <Text>Hello world</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
